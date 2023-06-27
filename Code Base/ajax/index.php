8d4dfcefabc4430d85637129f688506145bc316a
Anjit Sibakoti <asibakoti@ren.org.np>
2023-06-27 20:22:07 +0545

    Code Base Added

---
+++ Code Base/ajax/index.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+    <title>AJAX Test Page</title>
+</head>
+<body>
+
+<p><b>Start typing a name in the input field below:</b></p>
+<form action="">
+  <label for="fname">First name:</label>
+  <input type="text" id="fname" name="fname" onkeyup="showHint(this.value)">
+</form>
+<p>Suggestions: <span id="txtHint"></span></p>
+
+<script>
+function showHint(str) {
+  if (str.length == 0) {
+    document.getElementById("txtHint").innerHTML = "";
+    return;
+  } else {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("txtHint").innerHTML = this.responseText;
+      }
+    };
+    xmlhttp.open("GET", "gethint.php?q=" + str, true);
+    xmlhttp.send();
+  }
+}
+</script>
+
+
+</body>
+</html>
